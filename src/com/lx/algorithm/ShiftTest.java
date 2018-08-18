f6737c1ba58f5eb93c5b7823f51ddf18c5ee79ce
LiXing <2212640110@qq.com>
2018-08-18 18:19:06 +0800

    新增移位运算符测试.

---
+++ src/com/lx/algorithm/ShiftTest.java
@@ -0,0 +1,13 @@
+package com.lx.algorithm;
+/**
+ * title： com.lx.algorithm
+ * @author： lixing
+ * date： 2018/8/18 18:06
+ * description：移位运算符,<<向左移*2,>>向右移/2
+ */
+public class ShiftTest {
+    public static void main(String[] args) {
+        int a=7;
+        System.out.println(a>>>1);
+    }
+}
