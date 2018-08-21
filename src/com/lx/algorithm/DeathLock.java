7bd8c75ea6cd1b1a0e4bcdfdbd8c8f742e1fef14
LiXing <2212640110@qq.com>
2018-08-21 16:09:31 +0800

    新增多线程死锁样例代码和检测方法.

--- src/com/lx/algorithm/DeathLock.java
+++ src/com/lx/algorithm/DeathLock.java
@@ -3,7 +3,10 @@ package com.lx.algorithm;
  * title： com.lx.algorithm
  * @author： lixing
  * date： 2018/8/20 11:58
- * description：Java多线程死锁,线程A持有了锁A,等待获取锁B.
+ * description：Java多线程死锁,线程A持有了锁A,等待获取锁B.所对象.
+ * Synchronized的锁重入,防止死锁.
+ * A线程调用对象加入synchronized关键字的方法时,就获得了对象锁.
+ * B线程调用的使用就只能等待了.因为一个对象一把锁.
  */
 public class DeathLock {
     public static void main(String[] args) {
@@ -13,14 +16,14 @@ public class DeathLock {
             @Override
             public void run() {
                 synchronized (a){
-                    System.out.println("now in lock a class");
+                    System.out.println("now in lockA a class");
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     synchronized (b){
-                        System.out.println("now in lock b class");
+                        System.out.println("now in lockA b class");
                     }
                 }
             }
@@ -29,14 +32,14 @@ public class DeathLock {
             @Override
             public void run() {
                 synchronized (b){
-                    System.out.println("now in lock b class");
+                    System.out.println("now in lockB b class");
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(1);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     synchronized (a){
-                        System.out.println("now in lock a class");
+                        System.out.println("now in lockB a class");
                     }
                 }
             }
